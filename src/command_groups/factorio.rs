59099d80d6bbf199896584366217298a1db77197
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-03-26 18:04:13 +0300

    Added function for scheduled modlist and improved process_mod
    NOw it requires just a channel id instead of a whole message

--- src/command_groups/factorio.rs
+++ src/command_groups/factorio.rs
@@ -79,7 +79,7 @@ struct InfoJson {
 #[aliases(modstat, ms)]
 async fn mods_statistics(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     for mod_name in args.quoted().trimmed().iter::<String>() {
-        process_mod(ctx, msg, &mod_name?).await?;
+        process_mod(ctx, msg.channel_id, &mod_name?).await?;
     }
     Ok(())
 }
@@ -88,14 +88,21 @@ async fn mods_statistics(ctx: &Context, msg: &Message, mut args: Args) -> Comman
 #[aliases(ml)]
 async fn modlist(ctx: &Context, msg: &Message) -> CommandResult {
     for mod_name in MOD_LIST {
-        process_mod(ctx, msg, mod_name).await?;
+        process_mod(ctx, msg.channel_id, mod_name).await?;
     }
     Ok(())
 }
 
-async fn process_mod(ctx: &Context, msg: &Message, mod_name: &str) -> CommandResult {
+pub async fn scheduled_modlist(ctx: &Context, channel: ChannelId) -> CommandResult {
+    for mod_name in MOD_LIST {
+        process_mod(ctx, channel, mod_name).await?;
+    }
+    Ok(())
+}
+
+async fn process_mod(ctx: &Context, channel: ChannelId, mod_name: &str) -> CommandResult {
     let mod_data = get_mod_info(ctx, mod_name).await;
-    msg.channel_id.send_message(&ctx.http, |m| m.embed(|e| {
+    channel.send_message(&ctx.http, |m| m.embed(|e| {
         match mod_data {
             Ok(data) => construct_mod_embed(e, data),
             Err(_) => e.title("Mod not found")
