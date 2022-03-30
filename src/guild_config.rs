6b96355c446ab6419f35dbadb6577742a39394ad
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-03-30 17:53:40 +0300

    typo in comment

--- src/guild_config.rs
+++ src/guild_config.rs
@@ -201,7 +201,7 @@ impl GuildConfig {
                 if let Some(channel) = ic_edit.channel {
                     ic_data.channel_id = channel
                 }
-                // Using Vec::clear() would be more efficient, but thatt requires RwLock, which
+                // Using Vec::clear() would be more efficient, but that requires RwLock, which
                 // will require redoing code that uses mod_list_messages
                 if ic_type == InfoChannelType::ModList {
                     self.mod_list_messages = Arc::new(Vec::new());
